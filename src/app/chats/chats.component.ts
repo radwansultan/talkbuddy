523b57f596e855ddfeae4e40f5a3c17fadcf2cb6
Your Name <you@example.com>
2023-12-28 14:22:56 +0100

    Add edited subject

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -12,10 +12,7 @@ export class ChatsComponent {
   chatMessages: ChatMessage[] = [];
   chatMessageAdded: boolean = false;
   chatMessagedeleted: boolean = false;
-<<<<<<< HEAD
   chatMesageEdited: boolean = false;
-=======
->>>>>>> 5ef820351ad6303f61ac8924843b2f97e2e6726f
   isLoadingChatMessages: boolean = false;
   constructor(private chatsService: ChatsService) {}
 
@@ -24,10 +21,7 @@ export class ChatsComponent {
     this.getChatMessages();
     this.checkAddedMessages();
     this.checkdeletedMessages();
-<<<<<<< HEAD
     this.checkEditeddMessages();
-=======
->>>>>>> 5ef820351ad6303f61ac8924843b2f97e2e6726f
   }
 
   getChatMessages() {
@@ -60,7 +54,6 @@ export class ChatsComponent {
       console.log(deleted);
       setTimeout(() => {
         this.chatMessagedeleted = false;
-<<<<<<< HEAD
       }, 2000);
     });
   }
@@ -71,8 +64,6 @@ export class ChatsComponent {
       console.log(edited);
       setTimeout(() => {
         this.chatMesageEdited = false;
-=======
->>>>>>> 5ef820351ad6303f61ac8924843b2f97e2e6726f
       }, 2000);
     });
   }
